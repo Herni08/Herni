112b6a39f5e43b53b38d1bf8d9077fb3b6b24ea2
Herni08 <herniherni947@gmail.com>
2021-06-02 19:32:15 +0700

    Task

---
+++ index.js
@@ -0,0 +1,31 @@
+// Button of contact me
+function show(obj) {
+    obj.innerHTML = "herniherni947@gmail.com";
+}
+
+function hide(obj) {
+    obj.innerHTML = "E-mail";
+}
+
+function show2(obj) {
+    obj.innerHTML = "@herni_hermayani";
+}
+
+function hide2(obj) {
+    obj.innerHTML = "Instagram";
+}
+
+function show3(obj) {
+    obj.innerHTML = "Permata Biru Regency Block AG 112";
+}
+
+function hide3(obj) {
+    obj.innerHTML = "Address";
+}
+
+//merubah text dan ganti warna
+function change() {
+    document.getElementById("show1").innerHTML = "I want to travel the world without using my own money. Laos, Malaysia and Thailand have been countries I've been to without the need to spend money. If you want to travel around Thailand, just tell me. I can be your guide because I have lived there for a year.";
+    document.getElementById("show1").style.color="black";
+    document.getElementById("show1").style.backgroundColor="yellow";
+}
